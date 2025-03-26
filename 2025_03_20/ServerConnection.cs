6f1ac69090c5067d00120a5bef2abdf396ad16eb
Molnár Márk Krisztián <molnar.mark.krisztian.21i@egressy.info>
2025-03-26 14:06:23 +0100

    Add project files.

---
+++ 2025_03_20/ServerConnection.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Windows;
+
+namespace _2025_03_20
+{
+    public class ServerConnection
+    {
+        HttpClient client = new HttpClient();
+        string serverUrl = "";
+        public ServerConnection(string serverUrl)
+        {
+            this.serverUrl = serverUrl;
+        }
+
+        public async Task<bool> Login(string username, string password)
+        {
+            string url = serverUrl + "/login";
+            
+            try
+            {
+                var jsonInfo = new
+                {
+                    loginUsername = username,
+                    loginPassword = password
+
+                };
+                string jsonStringified = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent sendThis = new StringContent(jsonStringified, Encoding.UTF8, "Application/json");
+                HttpResponseMessage response = await client.PostAsync(url, sendThis);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                JsonData data = JsonConvert.DeserializeObject<JsonData>(result);
+                Token.token = data.token;
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message);
+            }
+            return false;
+        }
+        public async Task<bool> Register(string username, string password)
+        {
+            string url = serverUrl + "/register";
+
+            try
+            {
+                var jsonInfo = new
+                {
+                    registerUsername = username,
+                    registerPassword = password
+
+                };
+                string jsonStringified = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent sendThis = new StringContent(jsonStringified, Encoding.UTF8, "Application/json");
+                HttpResponseMessage response = await client.PostAsync(url, sendThis);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                JsonData data = JsonConvert.DeserializeObject<JsonData>(result);
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message);
+            }
+            return false;
+        }
+
+        public async Task<List<string>> Profiles()
+        {
+            List<string> all = new List<string>();
+            string url = serverUrl + "/profiles";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                all = JsonConvert.DeserializeObject<List<JsonData>>(result).Select(item => item.username).ToList();
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message);
+            }
+
+            return all;
+        }
+
+        public async Task<bool> create(string name, int age)
+        {
+            string url = serverUrl + "/createPerson";
+
+            try
+            {
+                var jsonInfo = new
+                {
+                    createName = name,
+                    createAge = age
+
+                };
+                string jsonStringified = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent sendThis = new StringContent(jsonStringified, Encoding.UTF8, "Application/json");
+                HttpResponseMessage response = await client.PostAsync(url, sendThis);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                JsonData data = JsonConvert.DeserializeObject<JsonData>(result);
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message);
+            }
+            return false;
+        }
+
+        public async Task<List<string>> AllName()
+        {
+            List<string> all = new List<string>();
+            string url = serverUrl + "/AllNames";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                all = JsonConvert.DeserializeObject<List<JsonData>>(result).Select(item => item.name).ToList();
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message);
+            }
+
+            return all;
+        }
+
+        public async Task<List<string>> AllAge()
+        {
+            List<string> all = new List<string>();
+            string url = serverUrl + "/AllAges";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                all = JsonConvert.DeserializeObject<List<JsonData>>(result).Select(item => Convert.ToString(item.age)).ToList();
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message);
+            }
+
+            return all;
+        }
+
+        public async Task<bool> DeletePerson(string name)
+        {
+            string url = serverUrl + "/deletePerson";
+            var jsonInfo = new
+            {
+                deleteName = name
+        };
+            try
+            {
+                string jsonStringified = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent sendThis = new StringContent(jsonStringified, Encoding.UTF8, "Application/json");
+                HttpResponseMessage response = await client.PostAsync(url, sendThis);
+                response.EnsureSuccessStatusCode();
+                string resault = await response.Content.ReadAsStringAsync();
+                JsonData data = JsonConvert.DeserializeObject<JsonData>(resault);
+                return true;
+            }
+            catch (Exception e)
+            {
+            }
+
+            return false;
+        }
+        public async Task<bool> deleteAll()
+        {
+            string url = serverUrl + "/deleteAll";
+            try
+            {
+                HttpResponseMessage response = await client.PostAsync(url,null);
+                response.EnsureSuccessStatusCode();
+                string resault = await response.Content.ReadAsStringAsync();
+                JsonData data = JsonConvert.DeserializeObject<JsonData>(resault);
+                return true;
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.Message);
+            }
+
+            return false;
+        }
+        public async Task<bool> update(string name,int age)
+        {
+            string url = serverUrl + "/update";
+            var jsonInfo = new
+            {
+                updatename = name,
+                updateage = age
+
+            };
+            try
+            {
+                string jsonStringified = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent sendThis = new StringContent(jsonStringified, Encoding.UTF8, "Application/json");
+                HttpResponseMessage response = await client.PostAsync(url, sendThis);
+                response.EnsureSuccessStatusCode();
+                string resault = await response.Content.ReadAsStringAsync();
+                JsonData data = JsonConvert.DeserializeObject<JsonData>(resault);
+                return true;
+            }
+            catch (Exception e)
+            {
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 2025_03_20/Window1.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _2025_03_20
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        ServerConnection connection;
+        public Window1(ServerConnection connection)
+        {
+            InitializeComponent();
+            this.connection = connection;
+            Start();
+        }
+        async void Start()
+        {
+            List<string> names = await connection.AllName();
+            List<string> ages = await connection.AllAge();
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                string name = names[i];
+                string age = ages[i];
+
+                TextBlock nameTextBlock = new TextBlock
+                {
+                    Text = name,
+                    FontSize = 20,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+                NameStack.Children.Add(nameTextBlock);
+
+                TextBlock ageTextBlock = new TextBlock
+                {
+                    Text = age,
+                    FontSize = 20,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+                AgeStack.Children.Add(ageTextBlock);
+
+                Button deleteB = new Button
+                {
+                    Content = "X",
+                    FontSize = 10,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    Margin = new Thickness(0, 10, 0, 0)
+                };
+                Button editbutton = new Button
+                {
+                    Content = "Edit",
+                    FontSize = 10,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    Margin = new Thickness(10, 10, 0, 0)
+                };
+                deleteB.Click += async (s, e) =>
+                {
+                    bool result = await connection.DeletePerson(name);
+                    if (result)
+                    {
+                        NameStack.Children.Remove(nameTextBlock);
+                        AgeStack.Children.Remove(ageTextBlock);
+                        deleteButtonStack.Children.Remove(deleteB);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error deleting the person.");
+                    }
+                };
+                editbutton.Click += async (s, e) =>
+                {
+                    bool resault = await connection.update(name, Convert.ToInt32(age));
+                    if (resault)
+                    {
+                        NameInput.Text = "";
+                        AgeInput.Text = "";
+                        deleteB.Visibility = Visibility.Hidden;
+                    }
+                };
+
+                deleteButtonStack.Children.Add(deleteB);
+                deleteButtonStack.Children.Add(editbutton);
+            }
+
+
+
+
+        }
+
+        async void createPerson(object s, EventArgs e)
+        {
+            bool valami = await connection.create(NameInput.Text, Convert.ToInt32(AgeInput.Text));
+            if (valami)
+            {
+
+                MessageBox.Show("created person");
+                NameStack.Children.Clear();
+                AgeStack.Children.Clear();
+                deleteButtonStack.Children.Clear();
+                Start();
+            }
+            else
+            {
+                MessageBox.Show("Nem jó valami");
+            }
+        }
+        async void deleteallperson(object s, EventArgs e)
+        {
+            bool valami = await connection.deleteAll();
+            if (valami)
+            {
+                MessageBox.Show("Mindenki kitorolve");
+                NameStack.Children.Clear();
+                AgeStack.Children.Clear();
+                deleteButtonStack.Children.Clear();
+                Start();
+
+            }
+        }
+    }
+}
